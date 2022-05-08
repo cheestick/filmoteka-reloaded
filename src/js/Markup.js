545393ec354ad312e3ef1a08c971af40aa8278ce
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-08 12:37:33 +0300

    some markup added

---
+++ src/js/Markup.js
@@ -0,0 +1,19 @@
+export function card(props) {
+  const { title, poster_path, genre, release_date, vote_average } = props;
+  return `<div class="card">
+    <img class="poster" src="${poster_path}" alt="${title} poster"/>
+    <div class="description">
+        <h2 class="title">${title}</h2>
+        <p class="briefs">
+            <span class="genre">${genre.join(', ')}</span>
+            <span class="devider">&nbsp|&nbsp</span>
+            <span class="year">${release_date}</span> 
+            <span class="rating">${vote_average}</span>
+        </p>
+    </div>
+   </div>`;
+}
+
+export function cardCollection(collection) {
+  return collection.map(item => card(item)).join('');
+}
