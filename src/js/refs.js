812951b6eae68504c89be65b52aaf64a25173d2e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-08 17:48:24 +0300

    refs.js added

---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+const refs = {
+  logo: document.querySelector('.logo__button'),
+  home: document.querySelector('.home'),
+  library: document.querySelector('.library'),
+  search: document.querySelector('.search__form'),
+  collection: document.querySelector('#movie-collection'),
+  //   queue: document.querySelector('.queue__button'),
+  //   watched: document.querySelector('.watched__button'),
+};
