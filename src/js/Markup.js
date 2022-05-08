d3cbd54bcbe9b658ea29583b2b556f0763c60edf
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-08 18:49:00 +0300

    card markup changed

--- src/js/Markup.js
+++ src/js/Markup.js
@@ -1,12 +1,17 @@
+const posterURL = 'https://image.tmdb.org/t/p/';
 export function card(props) {
-  const { title, poster_path, genre, release_date, vote_average } = props;
+  const { id, title, poster_path, genre_ids, release_date, vote_average } = props;
   return `<li class="movie__card">
-   <a class="movie__link">
-      <img class="movie__poster" src="${poster_path}" alt="${title} poster"/>
+   <a class="movie__link" data-id="${id}">
+      <img class="movie__poster"
+           loading="lazy"
+           alt="${title} poster"
+           src="${posterURL}w342${poster_path}"
+           srcset="${posterURL}w342${poster_path} 1x, ${posterURL}w500${poster_path} 2x" />
       <div class="movie__description">
           <h2 class="movie__title">${title}</h2>
           <p class="movie__briefs">
-              <span class="movie__genre">${genre.join(', ')}</span>
+              <span class="movie__genre">${genre_ids.join(', ')}</span>
               <span class="movie__devider">&nbsp|&nbsp</span>
               <span class="movie__year">${release_date}</span> 
               <span class="movie__rating">${vote_average}</span>
@@ -21,8 +26,9 @@ export function cardCollection(collection) {
 }
 
 export function render(component, parent) {
+  console.log(component, parent);
   clear(parent);
-  parent.insertAjacenHTML('afterbegin', component);
+  parent.insertAdjacentHTML('afterbegin', component);
 }
 
 function clear(component) {
