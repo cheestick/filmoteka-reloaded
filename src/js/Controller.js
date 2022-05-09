ccff30215456f15eca74c809849f1adbdce66756
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-09 12:32:23 +0300

    rough local saving functionality added for genres and pagination

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -2,7 +2,18 @@ import FetchService from './FetchService';
 import GenreList from './GenreList';
 import MovieList from './MovieList';
 import * as Markup from './Markup';
+import LocalStorage from './LocalStorage';
 import refs from './refs';
+
+const userData = {
+  WATCHED: 'watched',
+  QUEUE: 'queue',
+};
+
+const serviceData = {
+  GENRES: 'genres',
+  PAGINATION: 'pagination',
+};
 
 // FetchService.trendingMovies()
 //   .then(res => (MovieList.movies = res.data.results))
@@ -13,12 +24,28 @@ import refs from './refs';
 
 class Controller {
   constructor() {
-    FetchService.trendingMovies().then(res => {
-      console.log(res);
-      const cardList = Markup.cardCollection(res.data.results);
+    FetchService.genre().then(res => {
+      // console.log(res);
+      LocalStorage.save(serviceData.GENRES, res.data.genres);
+    });
+    FetchService.trendingMovies().then(({ data }) => {
+      const { page, total_pages, total_results } = data;
+      const pagination = { page, total_pages, total_results };
+      LocalStorage.save(serviceData.PAGINATION, pagination);
+      const genre = LocalStorage.load(serviceData.GENRES);
+      const results = data.results.map(movie => {
+        movie.genre_ids = movie.genre_ids.slice(0, 2);
+        movie['genres'] = movie.genre_ids.map(id => genre.find(genre => genre.id === id).name);
+        return movie;
+      });
+      console.log(genre);
+      console.log(results);
+      const cardList = Markup.cardCollection(results);
       Markup.render(cardList, refs.collection);
     });
   }
+
+  async init() {}
 }
 
 export default new Controller();
