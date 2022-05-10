eef7abd596363380cc54be9a8085c972def43010
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-10 17:48:53 +0300

    svg server path for modal refined

--- src/js/Markup.js
+++ src/js/Markup.js
@@ -59,7 +59,8 @@ export function movieInfoModal(props) {
     <div class="modal__topper">
       <button class="modal__close">
         <svg class="close__icon">
-          <use href="/sprites.313395d6.svg#cross"></use>
+        <!-- <use href="/sprites.313395d6.svg#cross"></use> -->
+          <use href="/sprites.6bdc8d38.svg#cross"></use> 
         </svg>
       </button>
     </div>
