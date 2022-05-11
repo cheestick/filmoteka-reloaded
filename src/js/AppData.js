c0884b312b64293fd9ac2d74a552b494bcc55ab6
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-11 23:16:40 +0300

    app data schema added

---
+++ src/js/AppData.js
@@ -0,0 +1,11 @@
+export const appData = {
+  pagination: {
+    page: null,
+    current_page: null,
+    total_pages: null,
+    total_results: null,
+  },
+  genres: [],
+  movies: [],
+  library: [],
+};
